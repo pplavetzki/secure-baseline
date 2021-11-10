0ea978ecce64c9b789d58aedc59ffaf106af13ff
Paul Plavetzki <paul@pareidolia.io>
2021-11-10 15:55:55 -0600

    trying install first
    Signed-off-by: Paul Plavetzki <paul@pareidolia.io>

--- terraform/08-GitOps-flux/flux.tf
+++ terraform/08-GitOps-flux/flux.tf
@@ -18,6 +18,7 @@ locals {
   ]
 }
 
+/*
 # SSH
 locals {
   known_hosts = "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
@@ -27,6 +28,7 @@ resource "tls_private_key" "main" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+*/
 
 resource "kubectl_manifest" "install" {
   for_each   = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
@@ -34,6 +36,7 @@ resource "kubectl_manifest" "install" {
   yaml_body  = each.value
 }
 
+/*
 resource "kubectl_manifest" "sync" {
   for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
   depends_on = [kubernetes_namespace.flux_system]
@@ -54,7 +57,8 @@ resource "kubernetes_secret" "main" {
     known_hosts    = local.known_hosts
   }
 }
-
+*/
+/*
 # GitHub
 resource "github_repository" "main" {
   name       = var.repository_name
@@ -94,3 +98,4 @@ resource "github_repository_file" "kustomize" {
   content    = data.flux_sync.main.kustomize_content
   branch     = var.branch
 }
+*/
--- terraform/08-GitOps-flux/main.tf
+++ terraform/08-GitOps-flux/main.tf
@@ -15,16 +15,20 @@ data "flux_install" "main" {
   target_path = var.target_path
 }
 
+/*
 data "flux_sync" "main" {
   target_path = var.target_path
   url         = "https://github.com/${var.github_owner}/${var.repository_name}.git"
   branch      = var.branch
 }
+*/
 
 data "kubectl_file_documents" "install" {
   content = data.flux_install.main.content
 }
 
+/*
 data "kubectl_file_documents" "sync" {
   content = data.flux_sync.main.content
 }
+*/
