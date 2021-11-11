dcf6f37090c4668397f0090fca4a688d84f01256
Paul Plavetzki <paul@pareidolia.io>
2021-11-11 09:04:57 -0600

    trying rg of kv
    Signed-off-by: Paul Plavetzki <paul@pareidolia.io>

--- terraform/05-Network-LZ/dns-zones.tf
+++ terraform/05-Network-LZ/dns-zones.tf
@@ -39,7 +39,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "lz_kv" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "hub_kv" {
   name                  = "hub_to_kvs"
-  resource_group_name   = data.terraform_remote_state.existing-hub.outputs.hub_rg_name
+  resource_group_name   = azurerm_resource_group.net-rg.name # data.terraform_remote_state.existing-hub.outputs.hub_rg_name
   private_dns_zone_name = azurerm_private_dns_zone.kv-dns.name
   virtual_network_id    = data.terraform_remote_state.existing-hub.outputs.hub_vnet_id
 }
