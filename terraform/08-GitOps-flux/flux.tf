cdaf36ed5f342458bbce0b53623551fe96f0a8c7
Paul Plavetzki <paul@pareidolia.io>
2021-12-01 16:29:15 -0600

    remove creating of git repo
    Signed-off-by: Paul Plavetzki <paul@pareidolia.io>

--- terraform/08-GitOps-flux/flux.tf
+++ terraform/08-GitOps-flux/flux.tf
@@ -20,18 +20,6 @@ locals {
 
 }
 
-/*
-# SSH
-locals {
-  known_hosts = "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
-}
-
-resource "tls_private_key" "main" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-*/
-
 resource "kubectl_manifest" "install" {
   for_each   = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
   depends_on = [kubernetes_namespace.flux_system]
@@ -52,26 +40,19 @@ resource "kubernetes_secret" "main" {
     name      = data.flux_sync.main.secret
     namespace = data.flux_sync.main.namespace
   }
- /*
-  data = {
-    identity       = tls_private_key.main.private_key_pem
-    "identity.pub" = tls_private_key.main.public_key_pem
-    known_hosts    = local.known_hosts
-  }
-  */
 }
 
 # GitHub
-resource "github_repository" "main" {
-  name       = var.repository_name
-  visibility = var.repository_visibility
-  auto_init  = true
-}
+# resource "github_repository" "main" {
+#   name       = var.repository_name
+#   visibility = var.repository_visibility
+#   auto_init  = true
+# }
 
-resource "github_branch_default" "main" {
-  repository = github_repository.main.name
-  branch     = var.branch
-}
+# resource "github_branch_default" "main" {
+#   repository = github_repository.main.name
+#   branch     = var.branch
+# }
 
 # resource "github_repository_deploy_key" "main" {
 #   title      = "staging-cluster"
@@ -80,23 +61,23 @@ resource "github_branch_default" "main" {
 #   read_only  = true
 # }
 
-resource "github_repository_file" "install" {
-  repository = github_repository.main.name
-  file       = data.flux_install.main.path
-  content    = data.flux_install.main.content
-  branch     = var.branch
-}
+# resource "github_repository_file" "install" {
+#   repository = github_repository.main.name
+#   file       = data.flux_install.main.path
+#   content    = data.flux_install.main.content
+#   branch     = var.branch
+# }
 
-resource "github_repository_file" "sync" {
-  repository = github_repository.main.name
-  file       = data.flux_sync.main.path
-  content    = data.flux_sync.main.content
-  branch     = var.branch
-}
+# resource "github_repository_file" "sync" {
+#   repository = github_repository.main.name
+#   file       = data.flux_sync.main.path
+#   content    = data.flux_sync.main.content
+#   branch     = var.branch
+# }
 
-resource "github_repository_file" "kustomize" {
-  repository = github_repository.main.name
-  file       = data.flux_sync.main.kustomize_path
-  content    = data.flux_sync.main.kustomize_content
-  branch     = var.branch
-}
+# resource "github_repository_file" "kustomize" {
+#   repository = github_repository.main.name
+#   file       = data.flux_sync.main.kustomize_path
+#   content    = data.flux_sync.main.kustomize_content
+#   branch     = var.branch
+# }
